2e7112f1289f246040efeea755128d13b01225da
sivaprasadram <sivaprasadv9323@gmail.com>
2023-04-27 14:30:09 +0530

    List or in Crud Operatation R part Complete I mean Read part

--- Employee_CRUD_Scaffolding/Controllers/HomeController.cs
+++ Employee_CRUD_Scaffolding/Controllers/HomeController.cs
@@ -10,21 +10,13 @@ namespace Employee_CRUD_Scaffolding.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            TutorialsCS _context = new TutorialsCS();
+
+
+            var listofData = _context.Employees.ToList();
+            return View(listofData);
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
